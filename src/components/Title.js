2a01ef189057f0272d37f2bfd1152cfac0093b03
Eddie Haddican <edwardhaddican@gmail.com>
2021-01-21 11:53:00 -0500

    added subheader

--- src/components/Title.js
+++ src/components/Title.js
@@ -4,6 +4,7 @@ const Title = () => {
   return (
     <div >
         <h1 className="main_title">The Lift Apprentice</h1>
+        <h3 className="main_title">The Assistant Every Mechanic Needs</h3>
     </div>
   );
 };
